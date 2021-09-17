8506bdf0a5ba1d55ca2ede30eb9e7663e05d701a
Sergey Yelin <elinsn@gmail.com>
2021-09-17 15:39:54 +0300

    Add woorl

---
+++ formula/woorl.rb
@@ -0,0 +1,22 @@
+class Woorl < Formula
+  desc "Woody cURL"
+  homepage "https://github.com/rbkmoney/woorl"
+  url "https://github.com/rbkmoney/woorl/",
+    using: :git,
+    tag: "1.4"
+  license "Apache 2.0"
+  bottle :unneeded
+
+  depends_on "erlang"
+  depends_on "git" => :build
+  depends_on "rebar3" => :build
+
+  def install
+    system "rebar3", "escriptize"
+    bin.install "_build/default/bin/woorl" => "woorl"
+  end
+
+  test do
+    system "#{bin}/woorl", "-h"
+  end
+end

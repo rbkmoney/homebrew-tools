0d9103db5e2feb3dbdd89f0773bd93a25902062e
Sergey Yelin <s.elin@rbkmoney.com>
2021-09-29 11:39:44 +0300

    Add custom thrift compiler for woorl (#4)

---
+++ formula/rbkthrift.rb
@@ -0,0 +1,75 @@
+class Rbkthrift < Formula
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  version "0.14.1-rbk"
+  url "https://github.com/rbkmoney/thrift.git",
+    using: :git,
+    revision: "c6c39e7b377bb36440cbc55d82ee30f8e7e395bc"
+  license "Apache 2.0"
+  bottle :unneeded
+
+  conflicts_with 'thrift', :because => 'both installs thrift'
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "bison" => :build
+  depends_on "boost" => [:build, :test]
+  depends_on "openssl@1.1"
+
+  def install
+    system "./bootstrap.sh" 
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+      --with-erlang
+      --without-haskell
+      --without-java
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-python
+      --without-ruby
+      --without-swift
+      --without-go
+      --without-rs
+      --without-lua
+      --without-nodejs
+      --without-c_glib
+      --without-py4
+    ]
+
+    ENV.cxx11 if ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.thrift").write <<~'EOS'
+      service MultiplicationService {
+        i32 multiply(1:i32 x, 2:i32 y),
+      }
+    EOS
+
+    system "#{bin}/thrift", "-r", "--gen", "cpp", "test.thrift"
+
+    system ENV.cxx, "-std=c++11", "gen-cpp/MultiplicationService.cpp",
+      "gen-cpp/MultiplicationService_server.skeleton.cpp",
+      "-I#{include}/include",
+      "-L#{lib}", "-lthrift"
+  end
+end
+
